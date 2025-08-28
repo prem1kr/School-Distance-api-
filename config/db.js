ad86951a2691b18fe974cfc58731e1c3e2ed0391
prem1kr <premkm608@.com>
2025-08-28 20:48:28 +0530

    certs

--- config/db.js
+++ config/db.js
@@ -1,29 +1,32 @@
 import mysql from "mysql2/promise";
 import dotenv from "dotenv";
+import fs from "fs";
 
 dotenv.config();
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST || "localhost",
-  user: process.env.DB_USER || "root",
-  password: process.env.DB_PASS || "123456",
-  database: process.env.DB_NAME || "schoolDB2",
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  ssl: {
+    ca: fs.readFileSync(process.env.DB_SSL)
+  },
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
 });
 
-
 const testConnection = async () => {
   try {
     const [rows] = await pool.query("SELECT 1 + 1 AS result");
-    console.log(" Database connected! Test result:", rows[0].result);
+    console.log("✅ Database connected! Test result:", rows[0].result);
   } catch (err) {
-    console.error(" Database connection failed:", err.message);
+    console.error("❌ Database connection failed:", err.message);
   }
 };
 
 testConnection();
-
 
 export default pool;
--- server.js
+++ server.js
@@ -10,4 +10,8 @@ app.use(express.json());
 app.use("/api", schoolRoutes);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+    
+
+});
